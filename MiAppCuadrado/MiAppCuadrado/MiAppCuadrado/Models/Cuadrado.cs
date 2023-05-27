ed36e70b50b029a3cb4c6e07d9a331f22346ca3c
jandro-10 <117784827+jandro-10@users.noreply.github.com>
2023-05-26 22:07:13 -0600

    segundo commit

---
+++ MiAppCuadrado/MiAppCuadrado/MiAppCuadrado/Models/Cuadrado.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiAppCuadrado.Models
+{
+    public class Cuadrado
+    {
+        public double Area { get; set; }
+        public double Perimetro { get; set; }
+        private double Lado;
+
+        public Cuadrado( double l) 
+        {
+            Lado = l;
+       
+            CalcularArea();
+            CalcularPerimetro();
+        
+        }
+
+
+        public void CalcularArea ()
+        {
+            Area = Lado * Lado;
+        }
+        public void CalcularPerimetro ()
+        {
+            Perimetro = 4 * Lado;
+        }
+        public override string ToString()
+        {
+            return $" Area: {Area} -  Perimetro: {Perimetro} ";
+        }
+    }
+}
---
+++ MiAppCuadrado/MiAppCuadrado/MiAppCuadrado/ViewModels/ViewModelCuadrado.cs
@@ -0,0 +1,55 @@
+﻿using MiAppCuadrado.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MiAppCuadrado.ViewModels
+{
+    public class ViewModelCuadrado : INotifyPropertyChanged
+    {
+
+        public ViewModelCuadrado() {
+        
+            Crear = new Command( () => {
+
+                Cuadrado  C= new Cuadrado(lado);
+                Resultado = C.ToString();
+
+            });
+        
+        
+        }
+
+
+        double lado;
+        public double Lado {
+            get => lado;
+            set
+            {
+                lado = value;
+                var args = new PropertyChangedEventArgs(nameof(lado));
+                PropertyChanged?.Invoke(this, args);
+            }        
+        }
+
+        string resultado;
+
+        public string Resultado
+        {
+            get => resultado;
+            set
+            {
+
+                resultado = value;
+                var args = new PropertyChangedEventArgs(nameof(resultado));
+                PropertyChanged?.Invoke(this, args);
+            }
+        }
+
+        public Command Crear { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
